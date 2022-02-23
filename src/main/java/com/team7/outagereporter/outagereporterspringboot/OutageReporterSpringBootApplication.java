cd87474d140d73f772a36283fe00e147c6dba16a
Michael Collins <4693027+Orsira@users.noreply.github.com>
2022-02-23 12:06:26 -0600

    Add generated project from Spring Initializr

---
+++ src/main/java/com/team7/outagereporter/outagereporterspringboot/OutageReporterSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.team7.outagereporter.outagereporterspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutageReporterSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutageReporterSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team7/outagereporter/outagereporterspringboot/OutageReporterSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team7.outagereporter.outagereporterspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutageReporterSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

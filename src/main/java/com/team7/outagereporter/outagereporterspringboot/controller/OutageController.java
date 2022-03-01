fd8b92e009d5ae859df11cfc47e818184e24ddda
Michael Collins <4693027+Orsira@users.noreply.github.com>
2022-02-28 22:57:54 -0600

    Add CrossOrigin annotation for local angular server

--- src/main/java/com/team7/outagereporter/outagereporterspringboot/controller/OutageController.java
+++ src/main/java/com/team7/outagereporter/outagereporterspringboot/controller/OutageController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class OutageController {
 
     private final OutageRepository repository;

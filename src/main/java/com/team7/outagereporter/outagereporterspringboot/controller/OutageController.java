7c0cc8ba3a1b2e44af68ef6725c6ab92320ef217
Michael Collins <4693027+Orsira@users.noreply.github.com>
2022-03-16 13:17:09 -0500

    Add HTTP PATCH method for OutageController

--- src/main/java/com/team7/outagereporter/outagereporterspringboot/controller/OutageController.java
+++ src/main/java/com/team7/outagereporter/outagereporterspringboot/controller/OutageController.java
@@ -1,5 +1,6 @@
 package com.team7.outagereporter.outagereporterspringboot.controller;
 
+import com.team7.outagereporter.outagereporterspringboot.advice.OutageNotFoundAdvice;
 import com.team7.outagereporter.outagereporterspringboot.exception.OutageNotFoundException;
 import com.team7.outagereporter.outagereporterspringboot.entity.Outage;
 import com.team7.outagereporter.outagereporterspringboot.repository.OutageRepository;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "moz-extension://906743b7-28d1-4dc0-a562-5246d630474a"})
 public class OutageController {
 
     private final OutageRepository repository;
@@ -31,6 +32,21 @@ public class OutageController {
     Outage one(@PathVariable Long id) {
         return repository.findById(id)
                 .orElseThrow(() -> new OutageNotFoundException(id));
+    }
+
+    @PatchMapping("/outages/{id}")
+    Outage updateOutage(@RequestBody Outage outage, @PathVariable Long id) {
+        return repository.findById(id)
+                .map(updatedOutage -> {
+                    updatedOutage.setFirstName(outage.getFirstName());
+                    updatedOutage.setLastName(outage.getLastName());
+                    updatedOutage.setStreetAddress(outage.getStreetAddress());
+                    updatedOutage.setZipCode(outage.getZipCode());
+                    updatedOutage.setEmail(outage.getEmail());
+                    updatedOutage.setComment(outage.getComment());
+                    return repository.save(updatedOutage);
+                }).orElseThrow(() -> new OutageNotFoundException(id));
+
     }
 
     @PutMapping("/outages/{id}")
